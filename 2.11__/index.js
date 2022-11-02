34dd99cde91ac77f46b05bb52ba23498ce154b73
Mwxyz2022 <rozhukvlad@gmail.com>
2022-11-02 11:51:32 +0200

    LC 2.11.22 methods

---
+++ 2.11__/index.js
@@ -0,0 +1,31 @@
+const numbersList1 = [1, 2, 3, 77, 5];
+
+//                          (!)
+// input: none;
+// output: number:
+console.log('BEFORE POP', numbersList1);
+const popRes = numbersList1.pop();
+console.log(popRes);
+
+console.log('AFTER POP', numbersList1);
+
+// input: element (any type)
+// output: array (new length) -- NEW LENGTH!!!!!
+
+const numbersList2 = [1, 2, 3, 4, 5];
+console.log('BEFORE PUSH', numbersList2);
+
+const pushRes = numbersList2.push(100, 'number', {}, []);
+console.log(pushRes);
+
+console.log('AFTER PUSH', numbersList2);
+
+// input: element (any type)
+// output: array (new length) -- NEW LENGTH!!!!!
+const numbersList3 = [1, 2, 3, 4, 5];
+console.log('BEFORE UNSHIFT', numbersList3);
+
+const unshiftRes = numbersList3.unshift(100, 'number', {}, []);
+console.log(unshiftRes);
+
+console.log('AFTER UNSHIFT', numbersList3);
