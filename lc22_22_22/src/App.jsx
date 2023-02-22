c3073e476dfd48de2555808c653fa9de943e5df1
Mwxyz2022 <rozhukvlad@gmail.com>
2023-02-22 14:18:59 +0200

    fix

--- lc22_22_22/src/App.jsx
+++ lc22_22_22/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import UsersList from './UserList';
 
-import { users } from './users.js';
+const users = [
+    { name: 'Tom', age: 20 },
+    { name: 'Bob', age: 100 },
+    { name: 'Rob', age: 32 },
+    { name: 'Rim', age: 45 },
+    { name: 'Skin', age: 27 },
+    { name: 'Bobby', age: 26 },
+    { name: 'Jack', age: 19 },
+    { name: 'Bom', age: 21 },
+    { name: 'DOM', age: 22 },
+    { name: 'Toretto', age: 60 },
+];
 
 const App = () => {
     return <UsersList users={users} />;
