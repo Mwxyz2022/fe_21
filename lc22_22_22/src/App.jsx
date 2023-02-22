c9f93b95c9a9a154f12590031697ec5b5bf8654d
Mwxyz2022 <rozhukvlad@gmail.com>
2023-02-22 14:52:30 +0200

    fix

--- lc22_22_22/src/App.jsx
+++ lc22_22_22/src/App.jsx
@@ -1,20 +1,7 @@
 import React from 'react';
 import UsersList from './UserList';
 
-const users = [
-    { name: 'Tom', age: 20 },
-    { name: 'Bob', age: 100 },
-    { name: 'Rob', age: 32 },
-    { name: 'Rim', age: 45 },
-    { name: 'Skin', age: 27 },
-    { name: 'Bobby', age: 26 },
-    { name: 'Jack', age: 19 },
-    { name: 'Bom', age: 21 },
-    { name: 'DOM', age: 22 },
-    { name: 'Toretto', age: 60 },
-    { name: 'Jane', age: 23 },
-    { name: 'Dina', age: 60 },
-];
+import { users } from './store.js';
 
 const App = () => {
     return <UsersList users={users} />;
---
+++ lc22_22_22/src/store.js
@@ -0,0 +1,15 @@
+export const users = [
+    { name: 'Tom', age: 20 },
+    { name: 'Bob', age: 100 },
+    { name: 'Rob', age: 32 },
+    { name: 'Rim', age: 45 },
+    { name: 'Skin', age: 27 },
+    { name: 'Bobby', age: 26 },
+    { name: 'Jack', age: 19 },
+    { name: 'Bom', age: 21 },
+    { name: 'DOM', age: 22 },
+    { name: 'Toretto', age: 60 },
+    { name: 'Jane', age: 23 },
+    { name: 'Dina', age: 60 },
+    { name: 'Sem', age: 60 },
+];
